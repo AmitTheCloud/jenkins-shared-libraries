be1dc3bb86602d1991245ca111b6013593723bf7
Amit Solanki <111967447+AmitTheCloud@users.noreply.github.com>
2025-11-21 17:58:27 +0530

    Simplify docker login command syntax

--- vars/push.groovy
+++ vars/push.groovy
@@ -4,9 +4,7 @@ withCredentials([usernamePassword(
                     passwordVariable: "dockerHubPass",
                     usernameVariable: "dockerHubUser"
                 )]) {
-                    sh """
-                    docker login -u "$dockerHubUser" -p "$dockerHubPass"
-                    """
+                    sh "docker login -u "$dockerHubUser" -p "$dockerHubPass"
                     sh "docker tag notes-app:latest ${dockerHubUser}/notes-app:latest"
                     sh "docker push ${dockerHubUser}/notes-app:latest"
                 }
