b62261ffe33ff844c06bddc5c78ac577351f895c
Amit Solanki <111967447+AmitTheCloud@users.noreply.github.com>
2025-11-21 17:57:09 +0530

    Add push.groovy for Docker Hub image management

---
+++ vars/push.groovy
@@ -0,0 +1,13 @@
+def call(String dockerHubUser, String Project, String ImageTag){
+withCredentials([usernamePassword(
+                    credentialsId: "dockerHubcred",
+                    passwordVariable: "dockerHubPass",
+                    usernameVariable: "dockerHubUser"
+                )]) {
+                    sh """
+                    docker login -u "$dockerHubUser" -p "$dockerHubPass"
+                    """
+                    sh "docker tag notes-app:latest ${dockerHubUser}/notes-app:latest"
+                    sh "docker push ${dockerHubUser}/notes-app:latest"
+                }
+}
