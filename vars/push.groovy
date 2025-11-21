e3b81faa23aba0d412f30ff9515fff2c62ee097d
Amit Solanki <111967447+AmitTheCloud@users.noreply.github.com>
2025-11-21 18:01:53 +0530

    Update Docker push script to use ImageTag and Project

--- vars/push.groovy
+++ vars/push.groovy
@@ -4,8 +4,8 @@ withCredentials([usernamePassword(
                     passwordVariable: "dockerHubPass",
                     usernameVariable: "dockerHubUser"
                 )]) {
-                    sh "docker login -u "$dockerHubUser" -p "$dockerHubPass"
-                    sh "docker tag notes-app:latest ${dockerHubUser}/notes-app:latest"
-                    sh "docker push ${dockerHubUser}/notes-app:latest"
+                    sh "docker login -u "${dockerHubUser}" -p "${dockerHubPass}"
+                    sh "docker tag notes-app:latest ${dockerHubUser}/notes-app:${ImageTag}"
+                    sh "docker push ${dockerHubUser}/${Project}:${ImageTag}"
                 }
 }
